e2364ed15ea2a93cb5309776ed5356437af0ddeb
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-06-29 10:22:35 +0200

    users route defined

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -7,6 +7,25 @@
 // 2. READ  ---> GET http://localhost:3001/users (+optional query params)
 // 3. READ  ---> GET http://localhost:3001/users/:userID 
 // 4. UPDATE  ---> PUT http://localhost:3001/users/:userID (+body)
-// 5. DELETE  ---> POST http://localhost:3001/users/:userID 
+// 5. DELETE  ---> DELETE http://localhost:3001/users/:userID 
 
 import express from "express";
+
+const usersRouter = express.Router()
+
+// 1.
+usersRouter.post()
+
+// 2.
+usersRouter.get()
+
+// 3.
+usersRouter.get()
+
+// 4.
+usersRouter.put()
+
+// 5.
+usersRouter.delete()
+
+export default usersRouter
