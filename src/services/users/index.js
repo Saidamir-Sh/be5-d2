8d613b67e36d70b5ec56bde132de92ddf5e597bd
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-06-29 10:11:57 +0200

    user endpoints

---
+++ src/services/users/index.js
@@ -0,0 +1,12 @@
+/* *********************    USERS ENDPOINT ************************** */
+
+
+/* ********************     USERS CRUD      ************************* */
+
+// 1. CREATE  ---> POST http://localhost:3001/users (+body)
+// 2. READ  ---> GET http://localhost:3001/users (+optional query params)
+// 3. READ  ---> GET http://localhost:3001/users/:userID 
+// 4. UPDATE  ---> PUT http://localhost:3001/users/:userID (+body)
+// 5. DELETE  ---> POST http://localhost:3001/users/:userID 
+
+import express from "express";
