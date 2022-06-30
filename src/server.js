10d55289c51bb1464ee3510c68aa9534b9063bb2
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-06-30 11:41:57 +0200

    bug fixes on importing modules

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 // const express = require("express") => OLD IMPORT SYNTAX
 import express from "express" // => new import syntax (enabled with "type": "module" in package.json)
+import listEndpoints from "express-list-endpoints"
+import usersRouter from "./services/users/index.js"
 
 const server = express()
 
 const port = 3001
+
+// > ************ ENDPOINTS ******************
+server.use('/users', usersRouter)
+console.table(listEndpoints(server))
 
 server.listen(port, () => {
     console.log(`Server running on port ${port}`)
