7723f0299e436fab53e1d1331e5f28389d1f65a7
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-07-01 10:27:14 +0200

    routes defined

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 // const express = require("express") => OLD IMPORT SYNTAX
 import express from "express" // => new import syntax (enabled with "type": "module" in package.json)
 import listEndpoints from "express-list-endpoints"
+import authorRouter from "./services/authors/index.js"
 import usersRouter from "./services/users/index.js"
 
 const server = express()
@@ -9,6 +10,8 @@ const port = 3001
 
 // > ************ ENDPOINTS ******************
 server.use('/users', usersRouter)
+server.use('/authors', authorRouter)
+
 console.table(listEndpoints(server))
 
 server.listen(port, () => {
--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -1,10 +1,32 @@
 import express from 'express'
 
-const authorRoutes = express.Router()
-
 // 1. READ  ---> GET http://localhost:3001/authors (+body)
 // 2. READ  ---> GET http://localhost:3001/authors/:authorID (+optional query params)
-// 3. READ  ---> GET http://localhost:3001/users/:userID 
-// 4. UPDATE  ---> PUT http://localhost:3001/users/:userID (+body)
-// 5. DELETE  ---> DELETE http://localhost:3001/users/:userID 
+// 3. CREATE  ---> POST http://localhost:3001/authors 
+// 4. UPDATE  ---> PUT http://localhost:3001/authors/:authorsID (+body)
+// 5. DELETE  ---> DELETE http://localhost:3001/authors/:authorsID 
 
+const authorRouter = express.Router()
+
+
+authorRouter.get('/', (req, res)  => {
+    res.send({message: 'I am author router'})
+})
+
+authorRouter.get('/:authorID', (req, res) => {
+    res.send('Hello Author ID')
+})
+
+authorRouter.post('/', (req, res) => {
+    res.send('I will add new author')
+})
+
+authorRouter.put('/:authorID', (req, res) => {
+    res.send('I will edit to existing author')
+})
+
+authorRouter.delete('/:authorID', (req, res) => {
+    res.send('I will delete the author')
+})
+
+export default authorRouter
